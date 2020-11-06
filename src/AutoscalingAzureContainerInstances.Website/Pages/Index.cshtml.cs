b49cb349dbaa18ea5a138be4ae9195b067e5bc09
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-11-06 08:46:41 +0100

    Provide portal website with Traffic Manager (#2)

---
+++ src/AutoscalingAzureContainerInstances.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AutoscalingAzureContainerInstances.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<IndexModel> _logger;
+        public string InstanceName { get; set; }
+
+        public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            InstanceName = _configuration["InstanceName"];
+        }
+    }
+}
---
+++ src/AutoscalingAzureContainerInstances.Website/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace AutoscalingAzureContainerInstances.Website
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder=> builder.AddEnvironmentVariables())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
